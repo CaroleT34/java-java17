076e2816e6854472ebe83847095754ca80435768
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-06-15 15:50:55 +0200

    test ok

--- src/test/java/java17/ex03/Function_03_Test.java
+++ src/test/java/java17/ex03/Function_03_Test.java
@@ -17,7 +17,14 @@ public class Function_03_Test {
     // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
     // TODO l'age de l'enfant est 0
     // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
+    BinaryOperator<Person> makeAChild = (pere, mere) -> {
+    	Person enfant = new Person();
+    	enfant.setLastname(pere.getLastname());
+    	enfant.setFirstname(pere.getFirstname() + " " + mere.getFirstname());
+    	enfant.setAge(0);
+    	System.out.println(enfant.getLastname() + " " + enfant.getFirstname()   + " a " + enfant.getAge() + " ans et son mdp est " + enfant.getPassword());
+    	return enfant;
+    };
     //  end::makeAChild[]
 
 
@@ -28,7 +35,7 @@ public class Function_03_Test {
         Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
         // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+        Person child = makeAChild.apply(father, mother);
 
         assert child.getFirstname().equals("John Aline");
         assert child.getLastname().equals("France");
--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -17,16 +17,16 @@ public class Function_06_Test {
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+        return "[age=" + supplier.get().getAge() +"]";
     }
     // end::formatAge[]
 
 
-    @Test
+	@Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+
+        String result = formatAge(() -> new Person("", "", 35, ""));
 
         assert result.equals("[age=35]");
     }
